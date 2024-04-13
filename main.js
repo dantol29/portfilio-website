aa26fcb235b9306cc72fa43058447b807587e0ed
dantol29 <dantol29@gmail.com>
2024-04-13 19:48:48 +0200

    add resizing window

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
 import * as THREE from 'three';
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1,);
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-
-camera.position.z = 5;
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+  
+  // Create scene
+  const scene = new THREE.Scene();
+  // Set scene's background
+  scene.background = new THREE.Color('skyblue');
+  
+  // Field of view
+  const fov = 75;
+  // Aspect ratio
+  const aspectRatio = window.innerWidth / window.innerHeight;
+  // Create camera
+  const camera = new THREE.PerspectiveCamera( fov, aspectRatio, 0.1, 1000 );
+  
+  // Create cube
+  const geometry = new THREE.BoxGeometry( 1, 1, 1,);
+  const material = new THREE.MeshStandardMaterial({ color: "purple" });
+  const cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+  
+  // set camera a bit further to see the cube
+  camera.position.set(0, 0, 3);
+  
+  const light = new THREE.DirectionalLight('yellow', 8);
+  light.position.set(10, 10, 10);
+  scene.add(light);
+  
+  // create renderer
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.physicallyCorrectLights = true;
+  
+  document.body.appendChild( renderer.domElement );
 
 function animate() {
 	requestAnimationFrame( animate );
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setPixelRatio(window.devicePixelRatio);
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
